1de7127ac08831306e3090ca238d2aae4ea527cc
jeroldindony <jeroldindony@gmail.com>
2025-02-04 23:40:37 +0530

    change

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Advice from './Advice';
 
 function App() {
   return (
     <div className="App">
-      
+      <Advice/>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ import Advice from './Advice';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <Advice/>
+    <App />
+    
   </React.StrictMode>
 );
 
